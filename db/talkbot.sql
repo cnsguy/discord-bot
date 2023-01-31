eb9b84b351e8ec25923360edcb527310057c14c2
cnsguy <82461731+cnsguy@users.noreply.github.com>
2023-01-31 09:25:36 +0100

    Drop talkbot_irc table

--- db/talkbot.sql
+++ db/talkbot.sql
@@ -11,13 +11,3 @@ CREATE TABLE talkbot_discord (
   UNIQUE(contentId, userId),
   FOREIGN KEY(contentId) REFERENCES talkbot_content(id)
 );
-
-CREATE TABLE talkbot_irc (
-  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  contentId INTEGER NOT NULL,
-  nick TEXT NOT NULL,
-  ident TEXT NOT NULL,
-  host TEXT NOT NULL,
-  UNIQUE(contentId, nick, ident, host),
-  FOREIGN KEY(contentId) REFERENCES talkbot_content(id)
-);
