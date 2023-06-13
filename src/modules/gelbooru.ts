cef56168e0b5cdd2662dfa27d414538a9598cdf2
cnsguy <82461731+cnsguy@users.noreply.github.com>
2023-06-14 00:29:11 +0200

    Add a gelbooru tag-based search command

--- src/modules/gelbooru.ts
+++ src/modules/gelbooru.ts
@@ -1,6 +1,6 @@
 import { Module } from '../module';
 import { Bot } from '../bot';
-import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder, SlashCommandStringOption } from 'discord.js';
 import assert from 'assert';
 
 interface ResultEntry {
@@ -9,7 +9,7 @@ interface ResultEntry {
 }
 
 interface Results {
-  readonly post: ResultEntry[];
+  readonly post?: ResultEntry[];
 }
 
 class CommandInfo {
@@ -19,61 +19,87 @@ class CommandInfo {
   }
 }
 
+async function handleImageCommon(interaction: ChatInputCommandInteraction, tags: string): Promise<void> {
+  await interaction.deferReply();
+
+  const response = await fetch(
+    `https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=${encodeURIComponent(tags)}`
+  );
+
+  if (response.status != 200) {
+    await interaction.followUp(`Failed to get image; server returned error code ${response.status}`);
+    return;
+  }
+
+  const json = JSON.parse(await response.text()) as Results;
+
+  if (json.post === undefined || json.post.length == 0) {
+    await interaction.followUp('No results.');
+    return;
+  }
+
+  const imageUrl = json.post[0].file_url;
+  const id = json.post[0].id;
+
+  if (imageUrl === undefined) {
+    await interaction.followUp(`Failed to get image; no results?`);
+    return;
+  }
+
+  const postUrl = `https://gelbooru.com/index.php?page=post&s=view&id=${id}`;
+  const embed = new EmbedBuilder().setTitle(`Post number ${id}`).setURL(postUrl).setImage(imageUrl);
+  await interaction.followUp({ embeds: [embed] });
+}
+
 export class GelbooruModule extends Module {
-  private readonly commands: Map<string, CommandInfo>;
+  private readonly premadeCommands: Map<string, CommandInfo>;
 
   private constructor(bot: Bot) {
     super();
 
-    this.commands = new Map();
-    this.commands.set(
+    this.premadeCommands = new Map();
+    this.premadeCommands.set(
       'explosion',
       new CommandInfo('rating:general sort:random megumin', 'Random Megumin picture from Gelbooru')
     );
-    this.commands.set(
+    this.premadeCommands.set(
       'useless',
       new CommandInfo('rating:general sort:random aqua_(konosuba)', 'Random Aqua picture from Gelbooru')
     );
-    this.commands.set(
+    this.premadeCommands.set(
       'illya',
       new CommandInfo('rating:general sort:random illyasviel_von_einzbern', 'Random Illya picture from Gelbooru')
     );
 
-    for (const [name, info] of this.commands.entries()) {
+    for (const [name, info] of this.premadeCommands.entries()) {
       const command = new SlashCommandBuilder().setName(name).setDescription(info.description).toJSON();
-      bot.registerSlashCommand(command, (interaction) => this.handleCommand(interaction));
+      bot.registerSlashCommand(command, (interaction) => this.premadeCommand(interaction));
     }
+
+    const command = new SlashCommandBuilder()
+      .setName('gelbooru')
+      .setDescription('Search gelbooru')
+      .addStringOption(
+        new SlashCommandStringOption().setName('tags').setDescription('Tags to search for').setRequired(true)
+      )
+      .toJSON();
+
+    bot.registerSlashCommand(command, (interaction) => this.gelbooruCommand(interaction));
   }
 
   public static load(bot: Bot): GelbooruModule {
     return new GelbooruModule(bot);
   }
 
-  private async handleCommand(interaction: ChatInputCommandInteraction): Promise<void> {
-    const info = this.commands.get(interaction.commandName);
+  private async premadeCommand(interaction: ChatInputCommandInteraction): Promise<void> {
+    const info = this.premadeCommands.get(interaction.commandName);
     assert(info !== undefined);
-    await interaction.deferReply();
+    await handleImageCommon(interaction, info.tags);
+  }
 
-    const response = await fetch(
-      `https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=${encodeURIComponent(info.tags)}`
-    );
-
-    if (response.status != 200) {
-      await interaction.followUp(`Failed to get image; server returned error code ${response.status}`);
-      return;
-    }
-
-    const json = JSON.parse(await response.text()) as Results;
-    const imageUrl = json.post[0].file_url;
-    const id = json.post[0].id;
-
-    if (imageUrl === undefined) {
-      await interaction.followUp(`Failed to get image; no results?`);
-      return;
-    }
-
-    const postUrl = `https://gelbooru.com/index.php?page=post&s=view&id=${id}`;
-    const embed = new EmbedBuilder().setTitle(`Post number ${id}`).setURL(postUrl).setImage(imageUrl);
-    await interaction.followUp({ embeds: [embed] });
+  private async gelbooruCommand(interaction: ChatInputCommandInteraction): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const tags = interaction.options.getString('tags')!;
+    await handleImageCommon(interaction, `rating:general sort:random ${tags}`);
   }
 }
