9883d1147f8ccbbcd3bb2863e68311362e7f4036
cnsguy <82461731+cnsguy@users.noreply.github.com>
2023-06-27 02:33:56 +0200

    Talkbot: import command

--- src/modules/talkbot.ts
+++ src/modules/talkbot.ts
@@ -1,13 +1,35 @@
 import { Module } from '../module';
 import { Bot, BotEventNames } from '../bot';
-import { Message } from 'discord.js';
+import {
+  ChatInputCommandInteraction,
+  Message,
+  SlashCommandAttachmentOption,
+  SlashCommandBuilder,
+  SlashCommandSubcommandBuilder,
+} from 'discord.js';
 import { TalkbotDatabase } from './talkbot/database';
+import { ManageGuild } from '../permission';
 
 export class TalkbotModule extends Module {
   private readonly database: TalkbotDatabase;
 
   private constructor(private readonly bot: Bot) {
     super();
+
+    const importSubcommand = new SlashCommandSubcommandBuilder()
+      .setName('import')
+      .setDescription('Import talkbot entries to the current guild')
+      .addAttachmentOption(
+        new SlashCommandAttachmentOption().setName('entries').setDescription('Talkbot entries').setRequired(true)
+      );
+
+    const talkbotCommand = new SlashCommandBuilder()
+      .setName('talkbot')
+      .setDescription('Talkbot commands')
+      .addSubcommand(importSubcommand)
+      .toJSON();
+
+    bot.registerSlashCommand(talkbotCommand, (interaction) => this.talkbotCommand(interaction));
     bot.on(BotEventNames.MessageCreate, (message) => void this.onMessageCreate(message));
     this.database = new TalkbotDatabase(this.bot.database);
     this.bot = bot;
@@ -15,6 +37,47 @@ export class TalkbotModule extends Module {
 
   public static load(bot: Bot): TalkbotModule {
     return new TalkbotModule(bot);
+  }
+
+  public async talkbotImportSubcommand(interaction: ChatInputCommandInteraction): Promise<void> {
+    if (!(await this.bot.checkInteractionPermissions(interaction, [ManageGuild]))) {
+      return;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const url = interaction.options.getAttachment('entries')!.url;
+    const response = await fetch(url);
+    const decoder = new TextDecoder('utf-8');
+
+    if (response.status != 200) {
+      await interaction.reply(`Failed to grab quotes from attachment; server returned error code ${response.status}`);
+      return;
+    }
+
+    const content = decoder.decode(await response.arrayBuffer());
+    let num = 0;
+
+    for (const quote of content.split('\n')) {
+      if (quote.length === 0) {
+        continue;
+      }
+
+      await this.database.newEntry(quote, interaction.user.id, interaction.guildId);
+      num += 1;
+    }
+
+    await interaction.reply(`Imported ${num} line(s).`);
+  }
+
+  public async talkbotCommand(interaction: ChatInputCommandInteraction): Promise<void> {
+    const subcommand = interaction.options.getSubcommand(true);
+
+    switch (subcommand) {
+      case 'import':
+        return this.talkbotImportSubcommand(interaction);
+      default:
+        throw new Error(`Invalid subcommand: ${subcommand}`);
+    }
   }
 
   public async onMessageCreate(message: Message): Promise<void> {
